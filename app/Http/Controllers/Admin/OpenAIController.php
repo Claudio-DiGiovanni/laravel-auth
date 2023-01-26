5399d857eae0b1e92fafda07a271fa404ede5fd1
Claudio-DiGiovanni <digioclaudio97@gmail.com>
2023-01-26 13:16:41 +0100

    test

--- app/Http/Controllers/Admin/OpenAIController.php
+++ app/Http/Controllers/Admin/OpenAIController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
-use OpenAI\OpenAI;
+use OpenAI\Client as OpenAI;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
@@ -10,27 +10,31 @@ use Illuminate\Http\Request;
 class OpenAIController extends Controller
 {
     public function generateText(Request $request) {
-        // Creare una nuova istanza di OpenAI
-        $openai = new OpenAI(env('OPENAI_SECRET'));
-
-        // Recuperare i parametri dalla richiesta
+        $url = 'https://api.openai.com/v1/engines/davinci/completions';
         $prompt = $request->input('prompt');
-        $model = $request->input('model', 'text-davinci-002');
+        $data = [
+            'prompt' => $prompt,
+            'max_tokens' => $request->input('max_tokens', 100),
+        ];
 
-        // Inviare la richiesta all'API OpenAI
-        $response = $openai->engines()->generate(
-            $model,
-            $prompt,
-            [
-                'max_tokens' => $request->input('max_tokens', 100)
-            ]
-        );
-
-        // Recuperare il testo generato dalla risposta
-        $generated_text = $response->get('choices')[0]['text'];
-
-        // Restituire il testo generato
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_POST, 1);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+        curl_setopt($curl, CURLOPT_HTTPHEADER, [
+            'Content-Type: application/json',
+            'Authorization: Bearer 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'
+        ]);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($curl);
+        $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        if($httpCode != 200) {
+            echo "Error: " . curl_error($curl);
+        }
+        curl_close($curl);
+        $response = json_decode($response, true);
+        $generated_text = $response['choices'][0]['text'];
         return response()->json(['generated_text' => $generated_text]);
     }
 }
-
