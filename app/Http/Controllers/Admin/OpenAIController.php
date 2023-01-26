ba49065272d9c0aa740a5136314e0208331cfbd3
Claudio-DiGiovanni <digioclaudio97@gmail.com>
2023-01-26 11:15:00 +0100

    problem with chatbot

--- app/Http/Controllers/Admin/OpenAIController.php
+++ app/Http/Controllers/Admin/OpenAIController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers\Admin;
 
+use OpenAI\OpenAI;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
-use OpenAI\OpenAI;
 
 class OpenAIController extends Controller
 {
